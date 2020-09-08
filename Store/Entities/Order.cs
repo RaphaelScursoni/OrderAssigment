5127fb4bc3642fb802539b4842bd350fbf089733
Raphael Scursoni <ra.scursoni@gmail.com>
2020-09-08 14:10:57 -0300

    Modificações de uniao das entidades

---
+++ Store/Entities/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Store.Entities.Enums;
+using System.Text;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+namespace Store.Entities
+{
+    class Order
+    {
+        public OrderStatus Status { get; set; }
+        public DateTime Moment { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(OrderStatus status, DateTime moment, Client client)
+        {
+            Status = status;
+            Moment = moment;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total() 
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items) 
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order Moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order Status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order Items: ");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total Price: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Store/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Store.Entities;
+using Store.Entities.Enums;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Client Data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Client NewClient = new Client(name, email, date);
+
+            Console.WriteLine("Enter Order Data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(status, DateTime.Now, NewClient);
+
+            Console.Write("How many itens to this order");
+            int QItens = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 1; i <= QItens; i++) {
+                Console.Write("Product Name: ");
+                string Pname = Console.ReadLine();
+                Console.Write("Product price: ");
+                double Pprice = double.Parse(Console.ReadLine());
+
+                Product product = new Product(Pname, Pprice);
+
+                Console.Write("Quantity: ");
+                int qtd = int.Parse(Console.ReadLine());
+
+                OrderItem Item = new OrderItem(qtd, Pprice, product);
+
+                order.AddItem(Item);
+            }
+
+            Console.WriteLine("ORDER SUMARY: ");
+            Console.WriteLine(order);
+
+        }
+    }
+}
